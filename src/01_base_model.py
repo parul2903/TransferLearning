e57b6c26cbaade79736739a806c0e0cb3e1aa994
Parul Sharma <parul3kin@gmail.com>
2022-02-19 18:02:19 +0530

    base model prep complete

--- src/01_base_model.py
+++ src/01_base_model.py
@@ -27,10 +27,10 @@ def main(config_path):
     mnist = tf.keras.datasets.mnist
     (x_train_full, y_train_full), (x_test, y_test) = mnist.load_data()
 
-    x_test = x_test / 255.
     x_train_full = x_train_full / 255.
-
-    x_valid, x_train = x_train_full[:5000] / 255., x_train_full[5000:] / 255.
+    x_test = x_test / 255.
+    
+    x_valid, x_train = x_train_full[:5000], x_train_full[5000:]
     y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
 
     # set the seed value
@@ -42,7 +42,7 @@ def main(config_path):
     LAYERS = [tf.keras.layers.Flatten(input_shape=[28, 28], name = "InputLayer"), 
             tf.keras.layers.Dense(300, name = "HiddenLayer1"),
             tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.Dense(300, name = "HiddenLayer2"),
+            tf.keras.layers.Dense(100, name = "HiddenLayer2"),
             tf.keras.layers.LeakyReLU(),
             tf.keras.layers.Dense(10, activation="softmax", name = "OutputLayer")]
 
@@ -57,9 +57,22 @@ def main(config_path):
     # summary of model 
     model.summary()
 
+    # train the model
+    history = model.fit(x_train, y_train, 
+                        epochs = 10, 
+                        validation_data = (x_valid, y_valid),
+                        verbose = 2)
+
+    # save the base model
+    model_dir_path = os.path.join("artifacts", "models")
+    create_directories([model_dir_path])
+
+    model_file_path = os.path.join(model_dir_path, "base_model.h5")
+    model.save(model_file_path)
 
 
-
+    logging.info(f"base model is saved at {model_file_path}")
+    logging.info(f"evaluation metrics {model.evaluate(x_test, y_test)}")
 
 
 if __name__ == '__main__':
